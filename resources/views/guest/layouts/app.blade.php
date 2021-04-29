801d38094f0a78b1156f3a8fed597fa6a3578f75
myari211 <appura16@gmail.com>
2021-04-29 16:27:01 +0700

    preloader

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -2,8 +2,34 @@
 <html lang="en">
 <head>
     @include('master.css')
+
+    <style type="text/css">
+      .preloader {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 9999;
+        background-color: #99daec;
+      }
+      .preloader .loading {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+        font: 14px arial;
+      }
+      </style>
 </head>
 <body class="sea">
+  <div class="preloader">
+    <div class="loading">
+      <div class="animated bounce infinite">
+        <img src="{{ asset('image/asyifa.png') }}" width="70">
+      </div>
+    </div>
+  </div>
 
   <html lang="en" class="full-height">
 
@@ -180,6 +206,11 @@
 </footer>
 
 @include('master.js')
+<script>
+$(document).ready(function(){
+  $(".preloader").fadeOut();
+});
+</script>
 
 
 
