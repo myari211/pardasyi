3c721402e4047728838552115d84e5e37023c492
myari211 <appura16@gmail.com>
2021-04-29 16:55:05 +0700

    about

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -3,53 +3,53 @@
 <div class="container">
   <div class="row" style="margin-top:150px;">
     <div class="col-lg-6">
-      <h1>About Company<hr></h1>
+      <h1>About Co<span class="badge badge-danger">mpany</span><hr></h1>
     </div>
   </div>
   <div class="row d-flex justify-content-end">
     <div class="col-lg-6">
       <span>
-        <h3><b>is a COMPANY</b></h3> engaged in the wholesale trade and transportation of goods of various types, to various destinations both <strong>local and international.</strong>
-        we are engaged in Export, Import and Trading. Established since <strong>September 26, 2019</strong>, we serve major international trade, either by land, sea or air.
+        <h3><span class="badge badge-warning">is a Co</span>mpany</h3> engaged in the wholesale trade and transportation of goods of various types, to various destinations both <span class="badge badge-success"><strong>local and international.</strong></span>
+        we are engaged in Export, Import and Trading. Established since <span class="badge badge-danger"><strong>September 26, 2019</strong></span>, we serve major international trade, either by land, sea or air.
       </span>
     </div>
   </div>
   <div class="row mt-4">
     <div class="col-lg-6">
       <span>
-        <h3><b>The ESTABLISHMENT</b></h3> of PT Assyifa with eternal blessings was carried out with full <strong>dedication</strong>, a strong desire to move <strong>forward</strong>, and a high spirit to become a company that can meet customer <strong>needs and satisfaction.</strong>
+        <h3><b>The es<span class="badge badge-primary">tablish</span>ment</b></h3> of PT Assyifa with eternal blessings was carried out with full <span class="badge badge-danger"><strong>dedication,</strong></span> a strong desire to move <span class="badge badge-warning"><strong class="text-white">forward,</strong></span> and a high spirit to become a company that can meet customer <span class="badge green-bg"><strong class="text-white">needs and satisfaction.</strong></span>
       </span>
     </div>
   </div>
   <div class="row d-flex justify-content-end mt-4">
     <div class="col-lg-6">
       <span>
-        <h3><b>the PROCESSION</b></h3> of the <strong>establishment</strong> of PT assyifa eternal blessings begins with the times and <strong>Renewal</strong>. PT Assyifa blessing eternal is a company that <strong>specializes</strong> in the wholesale trade of <strong>agricultural</strong> and animal products, fruits, forestry products, sports equipment, office stationery, steel, machinery, heavy equipment, and others with a custom <strong>clearance system.</strong>
+        <h3><b>the Proce<span class="badge badge-success">ssion</span></b></h3> of the <span class="badge badge-danger"><strong>establishment</strong></span> of PT assyifa eternal blessings begins with the times and <span class="badge badge-primary"><strong>Renewal.</strong></span> PT Assyifa blessing eternal is a company that <span class="badge badge-warning"><strong>specializes</strong></span> in the wholesale trade of <span class="badge badge-success"><strong>agricultural</strong></span> and animal products, fruits, forestry products, sports equipment, office stationery, steel, machinery, heavy equipment, and others with a custom <span class="badge green-bg"><strong class="text-white">clearance system.</strong></span>
       </span>
     </div>
   </div>
   <div class="row d-flex justify-content-end" style="margin-top:100px;">
     <div class="col-lg-6">
-      <h1>Vission<hr></h1>
+      <h1><span class="badge green-bg text-white">Vis</span>sion<hr></h1>
     </div>
   </div>
   <div class="row d-flex justify-content-end">
     <div class="col-lg-6">
-      <span>Making PT. Assyifa Berkah Abadi is a company that <strong>focuses</strong> on the field of Export - Import which is trusted, most advanced, <strong>productive</strong>, competitive, and <strong>ready to compete</strong> in the international arena</span>
+      <span>Making PT. Assyifa Berkah Abadi is a company that <span class="badge badge-warning"><strong>focuses</strong></span> on the field of Export - Import which is trusted, most advanced, <span class="badge green-bg"><strong class="text-white">productive</strong></span>, competitive, and <strong class="badge badge-danger">ready to compete</strong> in the international arena</span>
     </div>
   </div>
   <div class="row" style="margin-top:100px;">
     <div class="col-lg-6">
-      <h1>Mission<hr></h1>
+      <h1>Mis<span class="badge badge-danger">sion</span><hr></h1>
     </div>
   </div>
   <div class="row">
     <div class="col-lg-6">
       <ol class="p-0">
-        <li>Continue to provide the <strong>best and quality</strong> service to all customers and business partners</li>
-        <li>Improve service quality with a <strong>competent team</strong></li>
-        <li>Prioritizing <strong>speed and accuracy</strong> of handling</li>
-        <li>Prioritizing Time in accordance with <strong>customer expectations</strong></li>
+        <li>Continue to provide the <strong class="badge badge-danger">best and quality</strong> service to all customers and business partners</li>
+        <li>Improve service quality with a <strong class="badge badge-success">competent team</strong></li>
+        <li>Prioritizing <strong class="badge badge-warning">speed and accuracy</strong> of handling</li>
+        <li>Prioritizing Time in accordance with <strong class="badge badge-primary">customer expectations</strong></li>
       </ol>
     </div>
   </div>
