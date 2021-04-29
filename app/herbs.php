1d924601ed0e123b2b7e4e24cfa22a678f9d4df8
myari211 <appura16@gmail.com>
2021-04-29 15:54:35 +0700

    try herbs

--- app/herbs.php
+++ app/herbs.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Herbs extends Model
+class herbs extends Model
 {
     protected $table = "herbs";
 
--- database/seeds/HerbsSeeder.php
+++ database/seeds/HerbsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Herbs;
+use App\herbs as Herbs;
 
 class HerbsSeeder extends Seeder
 {
