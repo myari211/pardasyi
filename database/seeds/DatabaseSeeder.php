10608d359abfb0c8a022a4763794703db1386d45
myari211 <appura16@gmail.com>
2021-04-29 15:59:50 +0700

    fruits repair

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RiceSeeder::class);
         $this->call(HerbsSeeder::class);
         $this->call(MeatsSeeder::class);
-        $this->call(FruitSeeder::class);
+        $this->call(FruitsSeeder::class);
         $this->call(OthersSeeder::class);
     }
 }
