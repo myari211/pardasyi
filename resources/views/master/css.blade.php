5908cf4375f461e141dad3cf3bac50195180d22e
myari211 <appura16@gmail.com>
2021-04-29 13:12:23 +0700

    repair laravel mix

--- resources/views/master/css.blade.php
+++ resources/views/master/css.blade.php
@@ -11,5 +11,5 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 <!-- Material Design Bootstrap -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+<link rel="stylesheet" href="{{ asset('css/assyifa.css') }}" type="text/css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'public/css/style.css',
+],
+'public/css/assyifa.css').version();
+
+mix.scripts([
+    'public/js/assyifa.js',
+],
+'public/js/assyifaJS.js').version();
