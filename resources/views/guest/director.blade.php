ca28bb58089e4cd91ab49a17a34ff8907f61031e
myari211 <apputra16@gmail.com>
2022-08-18 10:51:09 +0700

    repair director

--- resources/views/guest/director.blade.php
+++ resources/views/guest/director.blade.php
@@ -20,23 +20,100 @@
             <div class="col-lg-12">
                 <div class="card z-depth-0" style="background-color: #F5F6FF">
                     <div class="card-body">
-                        <div class="row d-flex justify-content-center">
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-lg-6 d-flex justify-content-center align-items-center">
-                                                <i class="fas fa-user" style="font-size: 70px;"></i>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span style="font-size: 18px; font-weight: 600">Gierza Januar Parda S</span>
+                        <div class="d-none d-lg-block">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span style="font-size: 18px; font-weight: 600">Gierza Januar Parda S</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span>Director</span>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span>Director</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4 d-flex justify-content-center">
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span style="font-size: 18px; font-weight: 600">Arief Soegiyanto</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span>General Affair</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4 d-flex justify-content-center">
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span style="font-size: 18px; font-weight: 600">Musyarofah</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span>Admin</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4 d-flex justify-content-center">
+                                <div class="col-lg-6">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span style="font-size: 18px; font-weight: 600">Agus Eko</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-lg-12">
+                                                            <span>Staff Operasional</span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -45,74 +122,94 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="row mt-4 d-flex justify-content-center">
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-lg-6 d-flex justify-content-center">
-                                                <i class="fas fa-user" style="font-size: 70px;"></i>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span style="font-size: 18px; font-weight: 600">Arief Soegiyanto</span>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span>General Affair</span>
-                                                    </div>
+                        <div class="d-block d-lg-none">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center align-items-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row mt-4 d-flex justify-content-center">
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-lg-6 d-flex justify-content-center">
-                                                <i class="fas fa-user" style="font-size: 70px;"></i>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span style="font-size: 18px; font-weight: 600">Musyarofah</span>
-                                                    </div>
+                                            <div class="row mt-4">
+                                                <div class="col-12 d-flex justify-content-center">
+                                                    <span style="font-size: 18px; font-weight: 600">Gierza Januar Parda S</span>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span>Admin</span>
-                                                    </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-12 d-flex justify-content-center">
+                                                    <span>Director</span>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row mt-4 d-flex justify-content-center">
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-lg-6 d-flex justify-content-center">
-                                                <i class="fas fa-user" style="font-size: 70px;"></i>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span style="font-size: 18px; font-weight: 600">Agus Eko</span>
-                                                    </div>
+                            <div class="row mt-4">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <span>Staff Operasional</span>
-                                                    </div>
+                                            </div>
+                                            <div class="row mt-4">
+                                                <div class="col-lg-12 d-flex justify-content-center">
+                                                    <span style="font-size: 18px; font-weight: 600">Arief Soegiyanto</span>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-12 d-flex justify-content-center">
+                                                    <span>General Affair</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-12 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-4">
+                                                <div class="col-lg-12 d-flex justify-content-center">
+                                                    <span style="font-size: 18px; font-weight: 600">Musyarofah</span>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-12 d-flex justify-content-center">
+                                                    <span>Admin</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-lg-12">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-12 d-flex justify-content-center">
+                                                    <i class="fas fa-user" style="font-size: 70px;"></i>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-4">
+                                                <div class="col-12 d-flex justify-content-center">
+                                                    <span style="font-size: 18px; font-weight: 600">Agus Eko</span>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-12 d-flex justify-content-center">
+                                                    <span>Staff Operasional</span>
                                                 </div>
                                             </div>
                                         </div>
